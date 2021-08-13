cca48192c863c3d9bdf109926df90c3216f8e01a
Peter Malficent <dangcongtao1999tn@gmail.com>
2021-08-13 08:42:50 +0700

    fix main

--- ex3/main1.cpp
+++ ex3/main1.cpp
@@ -7,138 +7,137 @@
 
 using namespace std;
 
-int main () {
-    int n = 2;
-    int m = 3;
-
-    Person **list_person;
-    House *list_house;
-    
-    FILE *file = fopen("input.txt", "r");
+void input (Person **person, FILE *file_input, int n , int m) {
     int int_temp = 0;
     char char_temp[30];
-    
-    /* allocate */
-    list_person = new Person*[m];
-    for (int i = 0 ; i < n ; i ++) {
-        (list_person[i]) = new Person[m];
-    }
-    list_house = new House[n];
-
+    file_input = fopen("input.txt", "r");
     /*input */    
     for (int i = 0 ; i <n ; i ++) {
-
-        memset(char_temp,0,sizeof(char_temp));
-        fgets(char_temp,255,file);
-        list_house[i].set_house_id(char_temp);
-
-        int_temp = 0;
-        fscanf(file, "%d",&int_temp);
-        list_house[i].set_house_age(int_temp);
         
-        /* discard \0 in the file */
-        fgets(char_temp,255,file);
+        /* discard \0 in the file 
+        fgets(char_temp,255,file_input);*/
 
         for (int j = 0 ;j<m ; j ++) {
 
             memset(char_temp,0,sizeof(char_temp));
-            fgets(char_temp,255,file);
-            list_person[i][j].set_person_id(char_temp);
+            fgets(char_temp,255,file_input);
+            (person[i][j]).set_person_id(char_temp);
 
             memset(char_temp,0,sizeof(char_temp));
-            fgets(char_temp,255,file);  
-            list_person[i][j].set_person_name(char_temp);   
+            fgets(char_temp,255,file_input);  
+            person[i][j].set_person_name(char_temp);   
         }
-    }        
-    
-    fclose(file);
+    }
+    fclose(file_input);
+}
 
-    cout << "***in tt***\n";
+void print_all_infor (Person ** person, int n, int m, const char * string_push_in) {
+    cout << "***"<< string_push_in <<"***\n";
     /* out put */
     for (int i = 0 ; i <n ; i ++) {
-        list_house[i].print_all_house_infor();
+        
         cout<<endl;
         for (int j = 0 ;j<m ; j ++) {
-            list_person[i][j].print_all_person_infor();
+            person[i][j].print_all_person_infor();
         }
         cout<< endl;
         cout << endl;
     }
-    cout << "--- end in tt ---\n\n";
+    cout << "--- end " << string_push_in << "---\n\n";
+}
 
 
+void sort_by_person_id (Person **person, int n, int m) {
     /* sort sort by person */
     for (int i = 0 ; i < n ; i ++) {
         for (int j = 0 ;j < m-1 ; j ++) {
             for (int k = j+1 ; k < m ; k++) {
-                if (strcmp(list_person[i][j].get_person_id(), list_person[i][k].get_person_id()) > 0){
+                if (strcmp(person[i][j].get_person_id(), person[i][k].get_person_id()) > 0){
                     /* swap */
                     Person temp;
-                    temp.set_person_id(list_person[i][j].get_person_id());
-                    temp.set_person_name(list_person[i][j].get_person_name());
+                    temp.set_person_id(person[i][j].get_person_id());
+                    temp.set_person_name(person[i][j].get_person_name());
 
-                    list_person[i][j].set_person_id(list_person[i][k].get_person_id());
-                    list_person[i][j].set_person_name(list_person[i][k].get_person_name());
+                    person[i][j].set_person_id(person[i][k].get_person_id());
+                    person[i][j].set_person_name(person[i][k].get_person_name());
 
-                    list_person[i][k].set_person_id(temp.get_person_id());
-                    list_person[i][k].set_person_name(temp.get_person_name());
+                    person[i][k].set_person_id(temp.get_person_id());
+                    person[i][k].set_person_name(temp.get_person_name());
                 }
             }
         }
     }
+}
 
-    cout << "--- sort by person ---\n";
-    /* out put */
-    for (int i = 0 ; i <n ; i ++) {
-        list_house[i].print_all_house_infor();
-        cout<<endl;
-        for (int j = 0 ;j<m ; j ++) {
-            list_person[i][j].print_all_person_infor();
+void sort_by_person_name (Person **person, int n, int m){
+    /* sort sort by person */
+    for (int i = 0 ; i < n ; i ++) {
+        for (int j = 0 ;j < m-1 ; j ++) {
+            for (int k = j+1 ; k < m ; k++) {
+                if (strcmp(person[i][j].get_person_name(), person[i][k].get_person_name()) > 0){
+                    /* swap */
+                    Person temp;
+                    temp.set_person_id(person[i][j].get_person_id());
+                    temp.set_person_name(person[i][j].get_person_name());
+
+                    person[i][j].set_person_id(person[i][k].get_person_id());
+                    person[i][j].set_person_name(person[i][k].get_person_name());
+
+                    person[i][k].set_person_id(temp.get_person_id());
+                    person[i][k].set_person_name(temp.get_person_name());
+                }
+            }
         }
-        cout<< endl;
-        cout << endl;
     }
-    cout << "--- end sort by person ---\n" << endl;
+}
 
-    /* sort by house  */
+
+
+void sort_by_house (Person **person, int n , int m) {
     for (int i = 0 ; i <n-1 ; i ++) {
         for (int j = i+1 ;j<n ; j ++) {
-            if (strcmp(list_house[i].get_house_id(), list_house[j].get_house_id()) > 0) {
+            if (strcmp(person[i][0].get_person_id(), person[j][0].get_person_id()) > 0) {                
+                
                 /* swap house */
-                House temp;
-                temp.set_house_id(list_house[i].get_house_id());
-                temp.set_house_age(list_house[i].get_house_age());
-
-                list_house[i].set_house_id(list_house[j].get_house_id());
-                list_house[i].set_house_age(list_house[j].get_house_age());
-
-                list_house[j].set_house_id(temp.get_house_id());
-                list_house[j].set_house_age(temp.get_house_age());
-
-                /* swap person */
                 Person *temp_per;
-                temp_per = *&list_person[i];
-                list_person[i] = *&list_person[j];
-                (list_person[j]) = *(&temp_per);
+                temp_per = *&person[i];
+                person[i] = *&person[j];
+                person[j] = *(&temp_per);
 
                 /* pointer pointer to no where - to be safe */
                 temp_per = nullptr;
             }
         }
     }
+}
 
+int main () {
+    int n = 3;
+    int m = 3;
 
-    cout << "---- sort by house --- \n";
-    /* out put */
-    for (int i = 0 ; i <n ; i ++) {
-        list_house[i].print_all_house_infor();
-        cout<<endl;
-        for (int j = 0 ;j<m ; j ++) {
-            list_person[i][j].print_all_person_infor();
-        }
-        cout<< endl;
-        cout << endl;
+    Person **list_person;
+    
+    FILE *file;
+    
+    
+    /* allocate */
+    list_person = new Person*[m];
+    for (int i = 0 ; i < n ; i ++) {
+        (list_person[i]) = new Person[m];
     }
+    
+    input(list_person, file, n, m);
+    print_all_infor(list_person, n, m, "print all infor ");
+        
+    // sort_by_person_id(list_person, n, m);
+    // print_all_infor(list_person, n, m, " sort by person's id");
+
+    sort_by_person_name(list_person, n,m);
+    print_all_infor(list_person, n , m, " sort by person's name ");
+
+    sort_by_house(list_person, n,m);
+    print_all_infor(list_person, n , m, " sort by house ");
+
 
 
     /* anti leak */
@@ -148,7 +147,6 @@ int main () {
     }
     delete[] list_person;
     list_person = nullptr;
-
-    delete[] list_house;
-    list_house = nullptr;
+    
+    cout << "end..";
 }
